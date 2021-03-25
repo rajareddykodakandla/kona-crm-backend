6a9507234d1dfb298dd757d9cafdc95ce35222c3
raja <rajareddykodakandla>
2021-03-25 15:42:03 +0530

    CRR-4,5 Implement APIs using express js to store Registered user details in to database  Implement Login mechanism using JWT Tokens

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use("/user", userRoute);
+app.use("/v1/user", userRoute);
 
 mongoose.connect(process.env.URL, {
     useNewUrlParser: true,
